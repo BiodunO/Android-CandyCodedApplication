7b98a41ac549c76753aef8e66138cc8ad2969894
beeoh <abiolusanya@aol.com>
2019-11-03 01:29:24 +0000

    refactor(dockerfile): change ownership to projects

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -46,4 +46,12 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public  void  createPhoneIntent(View view){
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        Uri uriPhone = Uri.parse("tel:0123456789");
+        phoneIntent.setData(uriPhone);
+        startActivity(phoneIntent);
+
+    }
+
 }
